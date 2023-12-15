ec590f70031e6080fa46ce9fab0da3aa8a09d994
cclaus17 <99209070+cclaus17@users.noreply.github.com>
2023-12-15 01:57:59 -0500

    Add files via upload

---
+++ BlogPost.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BlogPost.aspx.cs" Inherits="Claus_Christian_HW4_MIST353.BlogPost" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <h2>Write a Blog Post</h2>
+        <asp:Label ID="lblTitle" runat="server" Text="Title"></asp:Label><br />
+        <asp:TextBox ID="txtTitle" runat="server" placeholder="Title"></asp:TextBox><br />
+        <asp:Label ID="lblUserName" runat="server" Text="UserName"></asp:Label><br />
+        <asp:TextBox ID="txtUserName" runat="server" placeholder="UserName"></asp:TextBox><br />
+        <asp:Label ID="lblContent" runat="server" Text="Content"></asp:Label><br />
+        <asp:TextBox ID="txtContent" runat="server" TextMode="MultiLine" Rows="5" placeholder="Content"></asp:TextBox><br />
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit Post" OnClick="btnSubmit_Click" /><br />
+        <hr />
+        <h2>Blog Posts</h2>
+        <asp:GridView ID="GridViewPosts" runat="server"></asp:GridView>
+    </div>
+</asp:Content>
---
+++ BlogPost.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Claus_Christian_HW4_MIST353
+
+{
+    public partial class BlogPost : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadPosts();
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+            string userName = txtUserName.Text; // Replace with actual user identity logic
+            string title = txtTitle.Text.Trim();
+            string content = txtContent.Text.Trim();
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["WeatherDataDB"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO BlogPosts (Title, Content, PostDate, Author) VALUES (@Title, @Content, @PostDate, @Author)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@Title", title);
+                    cmd.Parameters.AddWithValue("@Content", content);
+                    cmd.Parameters.AddWithValue("@PostDate", DateTime.Now);
+                    cmd.Parameters.AddWithValue("@Author", userName);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            LoadPosts();
+        }
+
+        private void LoadPosts()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["WeatherDataDB"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT Title, Content, PostDate, Author FROM BlogPosts ORDER BY PostDate DESC", conn))
+                {
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        GridViewPosts.DataSource = dt;
+                        GridViewPosts.DataBind();
+                    }
+                }
+            }
+        }
+    }
+}
